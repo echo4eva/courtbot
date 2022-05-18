b867c00722fe75d818e081c8e2496339dc4ed8a9
perilEcho <perilecho@gmail.com>
2022-05-17 21:01:58 -0700

    Played with message attribute logging

--- bot.py
+++ bot.py
@@ -5,4 +5,9 @@ from dotenv import load_dotenv  # used to hide vulnerable variables
 load_dotenv()
 
 bot = hikari.GatewayBot(token= os.getenv("TOKEN"))
+
+@bot.listen(hikari.GuildMessageCreateEvent)
+async def print_message(event):
+    print('USER: ', event.author, 'MSG: ', event.content, 'MSG ID: ', event.message_id)
+
 bot.run()
