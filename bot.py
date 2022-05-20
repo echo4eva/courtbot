333364b76b11987b52157b3dec887e92d91c8edd
perilEcho <perilecho@gmail.com>
2022-05-19 20:04:13 -0700

    Able to get attachments from message and send back

--- bot.py
+++ bot.py
@@ -11,11 +11,11 @@ bot = lightbulb.BotApp(
     prefix= "~"
 )
 
-@bot.command
-@lightbulb.command(name="testprefix", description="testing prefix")
-@lightbulb.implements(lightbulb.PrefixCommand)
-async def testing(ctx):
-    await ctx.respond(f"this prefix rocks!")
+# @bot.command
+# @lightbulb.command(name="testprefix", description="testing prefix")
+# @lightbulb.implements(lightbulb.PrefixCommand)
+# async def testing(ctx):
+#     await ctx.respond(f"this prefix rocks!")
 
 @bot.command
 @lightbulb.command(name="court", description="Go to court with someone")
@@ -45,41 +45,67 @@ async def accuse(ctx: lightbulb.PrefixContext):
         \nThe reason being accused: **{reason}**')
 
     await ctx.respond(f'Please provide evidence of a screenshot')
+
+@bot.command
+@lightbulb.command(name="evidence", description="Adds evidence to the court")
+@lightbulb.implements(lightbulb.PrefixCommand)
+async def evidence(ctx: lightbulb.PrefixContext):
+    # TODO: find better way to get URL from attachment
+    # Gets attachment from the messagel
+    attachment = ctx.event.message.attachments
+    # Checks if there's an attachment
+    # an empty attachment displays as []
+    if len(attachment) == 0:
+        await ctx.respond("There's no attatchment!")
+    else:
+        # The attachment comes as "Sequence[Attachment]", but not sure how to go through it
+        # as it only has one element in the list. When print, it says a lot of data including url, filename,
+        # but can only access the filename in the list as it has 1 element in it?
+        # So turned into a string, lol
+        attachment_string = str(attachment)
+        # Slice up to the point where we can get the URL
+        extracted_url = attachment_string.partition("url=")[2].split(",")[0].replace("'", "")
+        # Sends the URL in the chat
+        await ctx.respond(extracted_url)
+        
+        
+
     
     
-# Bot passive until event happens just using hikari
-@bot.listen(hikari.GuildMessageCreateEvent)
-async def print_message(event):
-    print('USER: ', event.author, 'MSG: ', event.content, 'MSG ID: ', event.message_id)
+# # Bot passive until event happens just using hikari
+# @bot.listen(hikari.GuildMessageCreateEvent)
+# async def print_message(event):
+#     print('USER: ', event.author, 'MSG: ', event.content, 'MSG ID: ', event.message_id)
+#     print(event.message.attachments) # able to get whatever attachment from the message
 
-# Bot command using lightbulb
-@bot.command
-@lightbulb.command(name= 'ping', description= 'pong')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def ping(ctx):
-    await ctx.respond('pong!')
+# # Bot command using lightbulb
+# @bot.command
+# @lightbulb.command(name= 'ping', description= 'pong')
+# @lightbulb.implements(lightbulb.SlashCommand)
+# async def ping(ctx):
+#     await ctx.respond('pong!')
 
-# Group commands using lightbulb
-@bot.command
-@lightbulb.command('group', 'this is a group')
-@lightbulb.implements(lightbulb.SlashCommandGroup)
-async def my_group(ctx):
-    pass
+# # Group commands using lightbulb
+# @bot.command
+# @lightbulb.command('group', 'this is a group')
+# @lightbulb.implements(lightbulb.SlashCommandGroup)
+# async def my_group(ctx):
+#     pass
 
-# Sub command using lightbulb
-@my_group.child
-@lightbulb.command('subcommand', 'this is a subcommand')
-@lightbulb.implements(lightbulb.SlashSubCommand)
-async def subcommand(ctx):
-    await ctx.respond('pog')
+# # Sub command using lightbulb
+# @my_group.child
+# @lightbulb.command('subcommand', 'this is a subcommand')
+# @lightbulb.implements(lightbulb.SlashSubCommand)
+# async def subcommand(ctx):
+#     await ctx.respond('pog')
 
-# Slash commands and options using lightbulb
-@bot.command
-@lightbulb.option('reason', 'why user go to court', type= str)
-@lightbulb.option('user', 'user to go to court against', type= str)
-@lightbulb.command('test', 'testing options')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def court(ctx):
-    await ctx.respond(f"{ctx.options.user}, has been accused because, {ctx.options.reason}.")
+# # Slash commands and options using lightbulb
+# @bot.command
+# @lightbulb.option('reason', 'why user go to court', type= str)
+# @lightbulb.option('user', 'user to go to court against', type= str)
+# @lightbulb.command('test', 'testing options')
+# @lightbulb.implements(lightbulb.SlashCommand)
+# async def court(ctx):
+#     await ctx.respond(f"{ctx.options.user}, has been accused because, {ctx.options.reason}.")
 
 bot.run()
